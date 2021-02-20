6dbee5a84f3fa8dd6481864f9bd11d0a98d4792b
Diego Ferreira Alves <diego.alves@ivoryit.com.br>
2021-02-20 15:42:11 -0300

    backend and frontend start

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,9 +1,26 @@
 import { Request, Response } from "express";
+import EmailService from "../services/EmailService";
 
 const users = [{ name: "Diego", email: "diegoferreira777@hotmail.com" }];
 
 export default {
   async index(req: Request, res: Response) {
     return res.json(users);
+  },
+
+  async create(req: Request, res: Response) {
+    const emailService = new EmailService();
+    emailService.sendMail({
+      to: {
+        name: "Diego Ferreira",
+        email: "diegoferreira777@hotmail.com",
+      },
+      message: {
+        subject: "Welcome to the system",
+        body: "Just a test",
+      },
+    });
+
+    return res.send();
   },
 };
